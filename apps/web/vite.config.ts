c05069060857523193591540c08d91f5ef2e8570
Andrew Koreykin <ankormoreankor@gmail.com>
2025-01-21 18:54:37 +0400

    fix ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') },
+      { find: '@libs', replacement: resolve(__dirname, 'src/lib') },
+    ],
+    // {
+    //   '@root': resolve(__dirname),
+    //   '@src': resolve('src'),
+    //   '@pages': resolve('src', 'pages'),
+    //   '@components': resolve('src', 'components'),
+    //   '@hooks': resolve('src', 'hooks'),
+    // },
+    preserveSymlinks: true,
+  },
+});
