9b8f840bfcf6c07521461ae27176e48fcc801fbe
Andrew Koreykin <ankormoreankor@gmail.com>
2025-01-23 17:17:28 +0400

    add redirect

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import ErrorBoundary from '@pages/ErrorPage/ErrorBoundary';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router';
 
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 
 import type { ReactNode } from 'react';
 import { ErrorPage } from '@pages/ErrorPage/ErrorPage';
@@ -11,13 +11,23 @@ import { CoinDetails } from '@pages/CoinDetails';
 
 const makeSuspended = (lazyNode: ReactNode) => <Suspense fallback={false}>{lazyNode}</Suspense>;
 
+const RedirectToRates = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(`/rates`);
+  }, []);
+
+  return false;
+};
+
 export const App = () => (
   <ErrorBoundary>
     <BrowserRouter>
       <Routes>
         <Route index path="/rates" element={<Rates />} />
-        <Route path="/rates/:coinName" element={<CoinDetails />} />
-        <Route path="/" element={<Rates />} />
+        <Route path="/rates/:coinName" element={makeSuspended(<CoinDetails />)} />
+        <Route path="/" element={<RedirectToRates />} />
         <Route path="*" element={makeSuspended(<ErrorPage />)} />
       </Routes>
     </BrowserRouter>
